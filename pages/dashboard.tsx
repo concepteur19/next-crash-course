7c0b958a0ff0f9d27fb53e315a380e30874eb4b9
concepteur19 <zobel.tchomgui@gmail.com>
2023-03-14 04:18:52 +0100

    14-03

---
+++ pages/dashboard.tsx
@@ -0,0 +1,10 @@
+import { Head } from 'next/document'
+import React from 'react'
+
+export default function dashboard() {
+    return (
+        <div>
+            <h1>Dasboard</h1>
+        </div>
+    )
+}
